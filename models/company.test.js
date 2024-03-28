92a7768a0daaf7a56da8030c70d906425c985e68
James Lee <jimhlee23@gmail.com>
2024-03-28 12:54:53 -0400

    finished testing of company model and routes

--- models/company.test.js
+++ models/company.test.js
@@ -60,28 +60,28 @@ describe("create", function () {
 
 describe("findAll", function () {
   test("works: no filter", async function () {
-    let companies = await Company.findAll();
+    let companies = await Company.findAll({});
     expect(companies).toEqual([
       {
         handle: "c1",
         name: "C1",
         description: "Desc1",
-        numEmployees: 1,
-        logoUrl: "http://c1.img",
+        num_employees: 1,
+        logo_url: "http://c1.img",
       },
       {
         handle: "c2",
         name: "C2",
         description: "Desc2",
-        numEmployees: 2,
-        logoUrl: "http://c2.img",
+        num_employees: 2,
+        logo_url: "http://c2.img",
       },
       {
         handle: "c3",
         name: "C3",
         description: "Desc3",
-        numEmployees: 3,
-        logoUrl: "http://c3.img",
+        num_employees: 3,
+        logo_url: "http://c3.img",
       },
     ]);
   });
@@ -92,7 +92,7 @@ describe("findAll", function () {
 
 describe("filter", function () {
   test("works: three things in query string", async function () {
-    let companies = await Company.filter({
+    let companies = await Company.findAll({
       nameLike: "c",
       minEmployees: 2,
       maxEmployees: 3
@@ -117,7 +117,7 @@ describe("filter", function () {
 
 
   test("works: one thing in query string", async function () {
-    let companies = await Company.filter({
+    let companies = await Company.findAll({
       maxEmployees: 2
     });
     expect(companies).toEqual([
@@ -140,7 +140,7 @@ describe("filter", function () {
 
   test("fails when minEmployees > maxEmployees", async function () {
     try {
-      let companies = await Company.filter({
+      let companies = await Company.findAll({
         maxEmployees: 2,
         minEmployees: 3
       });
